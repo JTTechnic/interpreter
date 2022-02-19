1d0a5a1404f36bc21b96cc7895bb993822fb1124
Jaron <jaron_zijlstra@outlook.com>
2022-02-19 16:14:22 +0100

    fix: use import type

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-import { Config } from "@jest/types";
+import type { Config } from "@jest/types";
 
 export default (): Config.InitialOptions => ({
 	testMatch: ["**/+(*.)+(spec|test).+(ts|js)?(x)"],
